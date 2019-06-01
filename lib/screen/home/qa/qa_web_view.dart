bfdfb9e1ced0374c0b74efb451791e310d4ff913
hirantha <sahanhirantha@gmail.com>
2019-06-01 14:52:13 +0530

    web view added for stackoverflow

---
+++ lib/screen/home/qa/qa_web_view.dart
@@ -0,0 +1,20 @@
+import 'package:fhack/data/model/stack_question.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class QAWebViewScreen extends StatelessWidget {
+  final Question question;
+
+  QAWebViewScreen({@required this.question});
+
+  @override
+  Widget build(BuildContext context) {
+    print(question.link);
+    return WebviewScaffold(
+      url: question.link,
+      appBar: AppBar(
+        title: Text("Test"),
+      ),
+    );
+  }
+}
