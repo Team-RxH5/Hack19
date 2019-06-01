a374a1df1e3a6a8da30dab1b231c119bb5a097aa
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-01 12:01:20 +0530

    Get message endpoint

--- lib/data/remote/gitter_api.dart
+++ lib/data/remote/gitter_api.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:fhack/data/model/message.dart';
 import 'package:fhack/util/config.dart';
@@ -5,8 +7,10 @@ import 'package:fhack/util/config.dart';
 class GitterAPI {
   Dio _dio;
 
-  static BaseOptions options = new BaseOptions(
-    baseUrl: GITTER_BASE_URL,
+  static Options _options = new Options(
+    headers: {
+      "Authorization": "Bearer fb291e0bfda8024a2f0a5eb2f97bc615c8cdfab5"
+    },
     // connectTimeout: 5000,
     // receiveTimeout: 3000,
   );
@@ -14,11 +18,15 @@ class GitterAPI {
   static const String ROOM_ID = "576c4d75c2f0db084a1f99ae";
 
   static const String MESSAGE_ENDPOINT =
-      "rooms/" + ROOM_ID + "/chatMessages?limit=50";
+      GITTER_BASE_URL + "rooms/" + ROOM_ID + "/chatMessages?limit=50";
 
-  Future<String> getMessages() {
-    return _dio.get(MESSAGE_ENDPOINT).then((Response res) {
-      return res.data.toString();
+  Future<List<Message>> getMessages() {
+    return _dio.get(MESSAGE_ENDPOINT, options: _options).then((Response res) {
+      return (res.data as List).map((i) {
+        return Message.fromJson(i);
+      }).toList();
+    }).catchError((err) {
+      print(err.toString());
     });
   }
 
@@ -29,5 +37,5 @@ class GitterAPI {
     return _gitterApi;
   }
 
-  GitterAPI._internal() : _dio = Dio(options);
+  GitterAPI._internal() : _dio = Dio();
 }
