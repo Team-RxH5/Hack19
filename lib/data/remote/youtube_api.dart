d51dd868608bb4b0eb5f22aa8069c9185713cfa4
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-01 15:01:29 +0530

    Implemented video api

---
+++ lib/data/remote/youtube_api.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:fhack/data/model/youtube_video.dart';
+import 'package:fhack/util/config.dart';
+
+class YoutubeAPI {
+  Dio _dio;
+
+  static const String CHANNEL_ID = "UCwXdFgeE9KYzlDdR7TG9cMw";
+
+  static const String VIDEO_ENDPOINT = YOUTUBE_BASE_URL +
+      "search?part=snippet&channelId=" +
+      CHANNEL_ID +
+      "&maxResults=25&key=AIzaSyC74XlyMXGlwBJLLUPwHFcCAlgvB0nIXSA";
+
+  Future<List<YoutubeVideo>> getVideos() {
+    return _dio.get(VIDEO_ENDPOINT).then((Response res) {
+      return (res.data["items"] as List).map((item) {
+        return YoutubeVideo.fromJson(item);
+      }).toList();
+    }).catchError((err) {
+      print(err.toString());
+    });
+  }
+
+  // Singleton
+  static final YoutubeAPI _youtubeApi = YoutubeAPI._internal();
+
+  factory YoutubeAPI() {
+    return _youtubeApi;
+  }
+
+  YoutubeAPI._internal() : _dio = Dio();
+}
---
+++ lib/screen/home/news/video_item.dart
@@ -0,0 +1,60 @@
+import 'package:fhack/data/model/youtube_video.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:markdown/markdown.dart' as Markdown;
+
+class VideoItem extends StatelessWidget {
+  final YoutubeVideo video;
+
+  const VideoItem({Key key, this.video}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+                    child: Image(
+                      image: NetworkImage(
+                        video.thumbnail,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 16.0, left: 16, right: 16),
+                    child: Text(
+                      video.title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8),
+                    child: Text(
+                      video.timestamp.toIso8601String(),
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
