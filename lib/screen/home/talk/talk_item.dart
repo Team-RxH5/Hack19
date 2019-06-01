a0c6a6a9e82d65224d827a5f2eb12ea5881092eb
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-01 13:25:18 +0530

    Updated styles

--- lib/screen/home/talk/talk_item.dart
+++ lib/screen/home/talk/talk_item.dart
@@ -11,10 +11,24 @@ class TalkItem extends StatelessWidget {
     return Column(
       children: <Widget>[
         ListTile(
-          title: Text(
-            message.user.displayName,
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                message.user.displayName,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 14),
+              ),
+              Spacer(),
+              Text(
+                message.sent.toIso8601String(),
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+            ],
           ),
-          subtitle: Text(message.text),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 4.0),
+            child: Text(message.text, style: TextStyle(color: Colors.black87, fontSize: 16),),
+          ),
         ),
         Divider(
           height: 1,
