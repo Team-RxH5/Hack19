9593fb88e947ddf144758985b87dfdfcfd74a52c
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-02 21:46:52 +0530

    Add join and chat sections

--- lib/screen/home/talk/talk_screen.dart
+++ lib/screen/home/talk/talk_screen.dart
@@ -1,6 +1,5 @@
 import 'package:Fluttery/data/model/gitter_message.dart';
 import 'package:Fluttery/data/remote/gitter_api.dart';
-import 'package:Fluttery/screen/common/header.dart';
 import 'package:Fluttery/screen/common/loader.dart';
 import 'package:Fluttery/screen/home/talk/talk_item.dart';
 import 'package:flutter/material.dart';
@@ -20,39 +19,61 @@ class TalkScreenState extends State<TalkScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[Header("Talk")];
-        },
-        body: ListView(
-          children: <Widget>[
-            FutureBuilder(
-              future: _api.getMessages(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<GitterMessage>> snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return Loader(false);
-                  default:
-                    if (snapshot.hasData) {
-                      return Column(
-                        children: snapshot.data
-                            .map((GitterMessage message) =>
-                                TalkItem(message: message))
-                            .toList(),
-                      );
-                    } else {
-                      return Text("Something went wrong!");
-                    }
-                }
-              },
-            ),
-          ],
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          ListView(
+            reverse: true,
+            children: <Widget>[
+              SizedBox(
+                height: 54,
+              ),
+              FutureBuilder(
+                future: _api.getMessages(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<GitterMessage>> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Loader(false);
+                    default:
+                      if (snapshot.hasData) {
+                        return Column(
+                          children: snapshot.data
+                              .map((GitterMessage message) =>
+                                  TalkItem(message: message))
+                              .toList(),
+                        );
+                      } else {
+                        return Text("Something went wrong!");
+                      }
+                  }
+                },
+              ),
+            ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: _buildJoin(context),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.person_add),
-        onPressed: () {},
+    );
+  }
+
+  Widget _buildJoin(BuildContext context) {
+    return Material(
+      color: Theme.of(context).accentColor,
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.black45,
+          child: Icon(Icons.add, color: Theme.of(context).accentColor),
+        ),
+        title: Text(
+          "Join the conversation with Gitter",
+          style: TextStyle(color: Colors.black54),
+        ),
       ),
     );
   }
@@ -60,42 +81,46 @@ class TalkScreenState extends State<TalkScreen> {
   Widget _buildTextComposer() {
     return IconTheme(
         data: new IconThemeData(color: Theme.of(context).accentColor),
-        child: new Container(
-          margin: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            children: <Widget>[
-              Flexible(
-                child: TextField(
-                  controller: _textEditingController,
-                  onChanged: (String text) {
-                    setState(() {
-                      _isComposing = text.length > 0;
-                    });
-                  },
-                  onSubmitted: _handleSubmitted,
-                  decoration:
-                      InputDecoration.collapsed(hintText: "Send Message"),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+          child: new Container(
+            margin: const EdgeInsets.only(left: 16.0),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    controller: _textEditingController,
+                    onChanged: (String text) {
+                      setState(() {
+                        _isComposing = text.length > 0;
+                      });
+                    },
+                    onSubmitted: _handleSubmitted,
+                    decoration:
+                        InputDecoration.collapsed(hintText: "Send Message"),
+                  ),
                 ),
-              ),
-              new Container(
-                margin: new EdgeInsets.symmetric(horizontal: 4.0),
-                child: Theme.of(context).platform == TargetPlatform.iOS
-                    ? new CupertinoButton(
-                        child: new Text("Send"),
-                        onPressed: _isComposing
-                            ? () =>
-                                _handleSubmitted(_textEditingController.text)
-                            : null,
-                      )
-                    : new IconButton(
-                        icon: new Icon(Icons.send),
-                        onPressed: _isComposing
-                            ? () =>
-                                _handleSubmitted(_textEditingController.text)
-                            : null,
-                      ),
-              )
-            ],
+                new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Theme.of(context).platform == TargetPlatform.iOS
+                      ? new CupertinoButton(
+                          child: new Text("Send"),
+                          onPressed: _isComposing
+                              ? () =>
+                                  _handleSubmitted(_textEditingController.text)
+                              : null,
+                        )
+                      : new IconButton(
+                          icon: new Icon(Icons.send),
+                          onPressed: _isComposing
+                              ? () =>
+                                  _handleSubmitted(_textEditingController.text)
+                              : null,
+                        ),
+                )
+              ],
+            ),
           ),
         ));
   }
